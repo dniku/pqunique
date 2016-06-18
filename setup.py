294546977bf7f36fb1c481232086f3a118bad9f3
Pastafarianist <mr.pastafarianist@gmail.com>
2016-06-18 23:26:18 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,45 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pqunique',
+
+    version='0.1',
+
+    description='Priority queue with unique items',
+    long_description=long_description,
+
+    url='https://github.com/Pastafarianist/pqunique',
+
+    author='Pastafarianist',
+    author_email='delonorm@gmail.com',
+
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='development queue priority',
+
+    py_modules=["pqunique"],
+)
